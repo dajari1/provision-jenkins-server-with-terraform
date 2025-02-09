1a51b6b8dc3dea4227d0efbb49fcb71c6c294473
clement2019 <ayeniayokunle@yahoo.co.uk>
2025-02-09 12:32:04 +0100

    files updated

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "jenkins-automation-server"
     region = "eu-west-2"
-    key = "devopskey2/terraform.tfstate"
+    key    = "jenkins-backend/terraform.tfstate"
   }
 }
--- new-jenkins-script.sh
+++ new-jenkins-script.sh
@@ -41,3 +41,18 @@ sudo ufw allow 8080
 sudo ufw allow OpenSSH
 sudo ufw enable
 sudo ufw status
+
+#another method of installing jenkins
+sudo apt update -y
+
+sudo apt upgrade -y 
+
+sudo apt install  -yopenjdk-17-jre
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y 
+sudo apt-get install jenkins -y
