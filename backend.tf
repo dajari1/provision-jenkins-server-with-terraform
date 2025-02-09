7250c8791469d6ac0e310390b95aeb2a83a543cc
unknown <ajaridennis@gmail.com>
2025-02-09 06:54:38 -0500

    provision of jenkins in terraform

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-automation-server"
-    region = "eu-west-2"
+    bucket = "denn-jenkins"
+    region = "us-east-1"
     key    = "jenkins-backend/terraform.tfstate"
   }
 }
